0f12ed1c1c82235984e51822ccdb77a8b8ea4db3
Gopal-G0 <heyitsmegopal@gmail.com>
2025-09-01 18:03:39 +0530

    added dependencies and added all the routes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.post('/signup', (req,res) => {
+
+});
+
+app.post('/login', (req,res) => {
+
+});
+
+app.post('/purchase', (req,res) => {
+
+});
+
+app.get('/getCourses', (req,res) => {
+
+});
+
+app.get('/getPurchasedCourses', (req,res) => {
+
+});
